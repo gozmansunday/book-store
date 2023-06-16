57363fce7120e332152be7123196a3be08b329a8
Goziem Sunday <thegzmn@gmail.com>
2023-06-16 22:19:43 +0100

    Tweak animation settings for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <AnimatePresence>
       <motion.main
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
       >
