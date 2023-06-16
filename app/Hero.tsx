2272f4ea8b2135a2e7273b01c81ef97b657f3343
Goziem Sunday <thegzmn@gmail.com>
2023-06-16 21:51:14 +0100

    Update hero section

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -14,21 +14,21 @@ const Hero = () => {
 
         {/* New Releases */}
         <div className='font-clash space-y-4 md:space-y-6 px-2'>
-          <h3 className='text-xl'>New releases</h3>
+          <h3 className='text-lg sm:text-xl'>New releases</h3>
 
           <div className='grid gap-14 lg:grid-cols-2 lg:gap-20'>
-            {/* Book Image and Info */}
+            {/* Book 1 Image and Info */}
             <section className='flex flex-col gap-8 md:flex-row'>
-              <Image className='w-full mx-auto md:w-56 shadow-lg shadow-neutral-700/50' src={arrowOfGod} alt="arrow-of-god" />
+              <Image className='w-full mx-auto md:w-52 shadow-lg shadow-neutral-700/50' src={arrowOfGod} alt="arrow-of-god" />
 
               <div className='space-y-3'>
                 <section className=' font-semibold'>
-                  <h4 className='text-xl'>Arrow of God</h4>
-                  <h4>Chinua Achebe</h4>
+                  <h4 className='text-lg sm:text-xl'>Arrow of God</h4>
+                  <h5 className='text-sm sm:text-base'>Chinua Achebe</h5>
                 </section>
                 
-                <section className='space-y-6'>
-                  <p className='line-clamp-[8] text-neutral-700'>
+                <section className='space-y-6 text-sm sm:text-base'>
+                  <p className='line-clamp-4 text-neutral-700 md:line-clamp-6'>
                     When Things Fall Apart ends, colonial rule has been introduced to Umuofia, and the character of the nation, its values, freedoms, religious and socio-political foundations have substantially and irrevocably been altered. Arrow of God, the second novel in Chinua Achebe’s The African Trilogy, moves the historical narrative forward. This time, the action revolves around Ezeulu, the headstrong chief priest of the god Ulu, which is worshipped by the six villages of Umuaro. The novel is a meditation on the nature, uses, and responsibility of power and leadership. Ezeulu finds that his authority is increasingly under threat from rivals within his nation and functionaries of the newly established British colonial government. Yet he sees himself as untouchable. He is forced, with tragic consequences, to reconcile conflicting impulses in his own nature—a need to serve the protecting deity of his Umuaro people; a desire to retain control over their religious observances; and a need to gain increased personal power by pushing his authority to the limits. He ultimately fails as he leads his people to their own destruction, and consequently, his personal tragedy arises. Arrow of God is an unforgettable portrayal of the loss of faith, and the downfall of a man in a society forever altered by colonialism.
                   </p>
                   <button className='flex items-center gap-2 border border-neutral-900 py-2 px-8 transition duration-200 hover:border-main-dark hover:bg-main-dark hover:text-neutral-100'>
@@ -38,18 +38,18 @@ const Hero = () => {
               </div>
             </section>
 
-            {/* Book Image and Info */}
+            {/* Book 2 Image and Info */}
             <section className='flex flex-col gap-8 md:flex-row'>
-              <Image className='w-full mx-auto md:w-56 shadow-lg shadow-neutral-700/50' src={americanah} alt="arrow-of-god" />
+              <Image className='w-full mx-auto md:w-52 shadow-lg shadow-neutral-700/50' src={americanah} alt="americanah" />
 
               <div className='space-y-3'>
                 <section className=' font-semibold'>
-                  <h4 className='text-xl'>Americanah</h4>
-                  <h4>Chimamanda Ngozi Adichie</h4>
+                  <h4 className='text-lg sm:text-xl'>Americanah</h4>
+                  <h5 className='text-sm sm:text-base'>Chimamanda Ngozi Adichie</h5>
                 </section>
                 
-                <section className='space-y-8'>
-                  <p className='line-clamp-[8] text-neutral-700'>
+                <section className='space-y-6 text-sm sm:text-base'>
+                  <p className='line-clamp-4 text-neutral-700 md:line-clamp-6'>
                     Ifemelu and Obinze are young and in love when they depart military-ruled Nigeria for the West. Beautiful, self-assured Ifemelu heads for America, where despite her academic success, she is forced to grapple with what it means to be Black for the first time. Quiet, thoughtful Obinze had hoped to join her, but with post–9/11 America closed to him, he instead plunges into a dangerous, undocumented life in London.
                   </p>
                   <button className='flex items-center gap-2 border border-neutral-900 py-2 px-8 transition duration-200 hover:border-main-dark hover:bg-main-dark hover:text-neutral-100'>
