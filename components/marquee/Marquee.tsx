0331375f46fe434a8daf8e668a797c3841a25537
Goziem Sunday <thegzmn@gmail.com>
2023-06-18 20:37:00 +0100

    Update homepage

--- components/marquee/Marquee.tsx
+++ components/marquee/Marquee.tsx
@@ -1,17 +1,23 @@
 import MarqueeContent from "./MarqueeContent";
 
-const Marquee = () => {
+interface Props {
+  content: string;
+}
+
+const Marquee = ({content}: Props) => {
   return (
     <div className="relative overflow-hidden">
       <section className="flex font-neue py-3 xs:py-4 md:py-6">
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
-        <MarqueeContent />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
+        <MarqueeContent content={content} />
       </section>
 
       <div className="absolute inset-y-0 inset-x-2  max-w-[96rem] mx-auto border-y-4 border-neutral-900" />
